38514af2722174e6b65e5acef013877f48266414
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2025-03-09 19:17:46 +0200

    restore main

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,13 @@ import { JWT } from 'google-auth-library';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 
 import config from './config';
-import { getTwoLevelsHeaders } from './utils/sheets';
-
-// import { google } from "googleapis";
 
 /*
   load test document and attempt to print the value of the cell in first row and first column in the tab named "Pages to generate"
 */
-try {
-  await processDocument();
-} catch (err) {
+processDocument().catch((err: unknown) => {
   console.error(err);
-}
+});
 
 async function loadDocument(): Promise<GoogleSpreadsheet> {
   const { documentId, client_email, private_key } = config;
@@ -23,12 +18,6 @@ async function loadDocument(): Promise<GoogleSpreadsheet> {
     key: private_key,
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   });
-
-  // const sheets = google.sheets({ version: "v4", auth: jwt });
-  // const sheet = await sheets.spreadsheets
-  //   .get({ spreadsheetId: documentId })
-  //   .then((data) => data.data.sheets);
-  // console.log({ sheet });
 
   const doc = new GoogleSpreadsheet(documentId, jwt);
 
@@ -61,10 +50,11 @@ async function loadDocument(): Promise<GoogleSpreadsheet> {
   }
 }
 
+const PAGES_TO_GENERATE = 'Pages to generate';
+
 async function processDocument() {
   const document = await loadDocument();
 
-  const PAGES_TO_GENERATE = '[NEW] Pages to generate - Togather 1';
   const pagesToGenerateSheet = document.sheetsByTitle[PAGES_TO_GENERATE];
 
   if (!pagesToGenerateSheet) {
@@ -73,52 +63,10 @@ async function processDocument() {
     );
   }
 
-  await getTwoLevelsHeaders(pagesToGenerateSheet);
+  await pagesToGenerateSheet.loadCells();
 
-  //   const { columnCount } = pagesToGenerateSheet;
+  const cell = pagesToGenerateSheet.getCell(0, 0);
 
-  //   // await pagesToGenerateSheet.loadHeaderRow(2);
-
-  //   // const rows = await pagesToGenerateSheet.getRows();
-  //   // const firstRow = rows[0];
-
-  //   // console.log(firstRow);
-
-  //   // // const cellC3 = pagesToGenerateSheet.get
-
-  //   await pagesToGenerateSheet.loadCells({
-  //     startRowIndex: 0,
-  //     endRowIndex: 2,
-  //   });
-
-  //   const firstLevelHeaders: {
-  //     startingColumnIdx: number;
-  //     value: string;
-  //     subheaders: string[];
-  //   }[] = [];
-
-  //   for (let i = 0; i < columnCount; i++) {
-  //     const value = getCellValue({ rowIndex: 0, columnIndex: i });
-
-  //     if (value) {
-  //       firstLevelHeaders.push({ startingColumnIdx: i, value, subheaders: [] });
-  //     }
-  //   }
-
-  //   firstLevelHeaders.forEach((firstLevelHeader, idx) => {
-  //     const nextFirstLevelHeader = firstLevelHeaders.at(idx + 1);
-  //     const endingIdx = nextFirstLevelHeader?.startingColumnIdx ?? columnCount;
-
-  //     for (let j = firstLevelHeader.startingColumnIdx; j < endingIdx; j++) {
-  //       const value = getCellValue({ rowIndex: 1, columnIndex: j });
-
-  //       if (value) {
-  //         firstLevelHeader.subheaders.push(value);
-  //       }
-  //     }
-  //   });
-
-  //   console.log(JSON.stringify(firstLevelHeaders, null, 4));
-
-  //   // prints the value of the cell in first row and first column
+  // prints the value of the cell in first row and first column
+  console.info(cell.value);
 }
